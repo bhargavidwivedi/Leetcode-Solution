408d9c4e6a4677efbd2c0a1a0ccaa0c6de8022e6
bhargavidwivedi <bhargavi.23mim10215@vitbhopal.ac.in>
2026-02-24 10:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (57.92%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        
+        int minRow = m;
+        int minCol = n;
+        if(ops.length == 0){
+            return m*n;
+        }
+
+        for(int [] op : ops){
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+        return minRow*minCol;
+    }
+}
